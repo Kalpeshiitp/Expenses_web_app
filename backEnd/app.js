12d674a8126197a5706324c0a9ae61148317e7db
Kalpeshiitp <kalpeshkumar267@gmail.com>
2023-10-17 09:04:32 +0530

    add login page

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const sequelize = require("./util/database");
-const SignUp = require('./models/signup');
+const User = require('./models/user');
 const cors = require('cors');
 
 const app = express()
@@ -15,17 +15,17 @@ app.post('/user/signup', async (req, res, next) => {
     const password = req.body.password;
 
     try {
-        const existingUser = await SignUp.findOne({ where: { email: email } });
+        const existingUser = await User.findOne({ where: { email: email } });
         if(existingUser){
            return  res.status(400).send("user already exist")
           
         }
-        const signup = await SignUp.create({
+        const user = await User.create({
             name: name,
             email: email,
             password: password
         });
-        res.status(201).send(signup);
+        res.status(201).send(user);
         console.log("Data successfully sent to database");
     } catch (err) {
         res.status(500).send("error posting the data to database " + err); // Fix the line here
