83a20373a64227dd8923ae7d9d45fa3fba826d6c
Kalpeshiitp <kalpeshkumar267@gmail.com>
2023-10-20 15:30:07 +0530

    use Transaction in post and delete request

--- backEnd/controller/expense.js
+++ backEnd/controller/expense.js
@@ -1,25 +1,32 @@
 const Expense = require('../models/expense');
 const User = require("../models/user");
+const sequelize = require('../util/database');
+
 
 exports.postExpense = async (req, res) => {
+  const t = await sequelize.transaction()
   try {
     const { money, description, type } = req.body;
     if (money === undefined || money.length === 0) {
       return res.status(400).json({ success: false, message: "Parameter missing" });
     }
-    const data = await Expense.create({ money: money, description: description, type: type, userId: req.user.id });
+   
+    const data = await Expense.create({ money: money, description: description, type: type, userId: req.user.id },{transaction:t});
 
     // Calculate the new totalExpense
     const newExpense = Number(money);
+    console.log("newExpense>>>>>",newExpense)
     const updatedTotalExpense = Number(req.user.totalExpense) + newExpense;
 
     // Update the user's totalExpense in the database
     await User.update({ totalExpense: updatedTotalExpense }, {
-      where: { id: req.user.id }
+      where: { id: req.user.id },transaction:t
     });
-
+    
+    await t.commit();
     res.status(201).json({ newExpenseDetail: data });
   } catch (err) {
+    await t.rollback();
     console.log(err);
     res.status(500).json({
       error: err.message,
@@ -39,6 +46,7 @@ exports.postExpense = async (req, res) => {
   }
 
   exports.deleteExpense = async(req,res)=>{
+    const t = await sequelize.transaction()
     const expenseId = req.params.id
     try{
   const expense = await Expense.findByPk(expenseId);
@@ -49,9 +57,19 @@ if (!expense) {
     console.log('expense.userId:', expense.userId); 
     return res.status(403).json({ error: 'You are not authorized to delete this expense' });
   }
-  await expense.destroy({where:{userId:req.user.id}});
+  
+  const expenseAmount =  expense.money
+  const user = await User.findByPk(req.user.id);
+  user.totalExpense -= expenseAmount;
+  await User.update({ totalExpense: user.totalExpense }, {
+    where: { id: req.user.id },
+    transaction: t,
+  });
+  await expense.destroy({where:{userId:req.user.id},transaction:t});
+  await t.commit();
   res.status(204).send()
     }catch(err){
+      await t.rollback();
         console.log(err)
       res.status(500).json({error:err.message})
     }
@@ -69,6 +87,7 @@ if (!expense) {
         return res.status(403).json({ error: 'You are not authorized to edit this expense' });
       }
    }catch(err){
+    await t.rollback();
   res.status(500).json({error:err.message})
    }
 
